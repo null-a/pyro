3cb7efc4e9d9e7bc18d4951ed8036567cf77cab6
null-a <null-a@users.noreply.github.com>
2018-02-26 15:27:06 +0000

    Draw extra border to indicate i=1.
    This is useful then the window is outside of the frame.

--- examples/dynair/dyn3d.py
+++ examples/dynair/dyn3d.py
@@ -555,7 +555,12 @@ def overlay_window_outlines(dynair, frame, z_where):
     return over(draw_window_outline(dynair, z_where), frame)
 
 def overlay_window_outlines_conditionally(dynair, frame, z_where, ii):
-    return over(draw_window_outline(dynair, z_where) * ii.view(-1, 1, 1, 1), frame)
+    batch_size = z_where.size(0)
+    presence_mask = ii.view(-1, 1, 1, 1)
+    borders = batch_expand(Variable(torch.Tensor([-0.08, 0, 0])), batch_size)
+    return over(draw_window_outline(dynair, borders) * presence_mask,
+                over(draw_window_outline(dynair, z_where) * presence_mask,
+                     frame))
 
 
 def latent_seq_to_tensor(arr):
