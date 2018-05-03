eade4d92bfa608518bc374b876c066307daf4c47
null-a <null-a@users.noreply.github.com>
2018-05-03 12:02:06 +0100

    CUDA fix.

--- examples/dynair/opt.py
+++ examples/dynair/opt.py
@@ -81,6 +81,12 @@ def opt_all(data, X_split, Y_split, cfg, args, output_path, log_to_cond):
     X_train, X_test = X_split
     Y_train, Y_test = Y_split
 
+    if args.cuda:
+        X_train = X_train.cuda()
+        Y_train = Y_train.cuda()
+        X_tes = X_test.cuda()
+        Y_test = Y_test.cuda()
+
     batch_size = X_train[0].size(0)
 
     # Produce visualisations for the first train & test data points
@@ -110,34 +116,30 @@ def opt_all(data, X_split, Y_split, cfg, args, output_path, log_to_cond):
         print()
         pp(dynair.cache_stats())
 
-    if args.cuda:
-        X_train.cuda()
-        Y_train.cuda()
-
     run_svi(dynair, list(zip(X_train, Y_train)), args.epochs, hook, output_path, args.s,
             elbo_scale=1.0/(cfg.seq_length*batch_size))
 
 
 def opt_bkg(data, X_split, Y_split, cfg, args, output_path, log_to_cond):
     vis = visdom.Visdom()
-    vae = VAE(ParamY(cfg), DecodeBkg(cfg), cfg.y_size)
+    vae = VAE(ParamY(cfg), DecodeBkg(cfg), cfg.y_size, use_cuda=args.cuda)
 
     X_train, _ = X_split
     # Extract backgrounds from the input sequences.
     batches = X_train.mode(2)[0].view(X_train.size()[0:2] + (-1,))
-    batch_size = batches.size(1)
+    if args.cuda:
+        batches = batches.cuda()
+
     vis_batch = batches[0, 0:10]
+    batch_size = batches.size(1)
 
     if args.vis > 0:
-        vis.images(vis_batch.view(-1, cfg.num_chan, cfg.image_size, cfg.image_size), nrow=10)
+        vis.images(vis_batch.cpu().view(-1, cfg.num_chan, cfg.image_size, cfg.image_size), nrow=10)
 
     def hook(epoch, batch, step):
         if args.vis > 0 and (step + 1) % args.vis == 0:
-            x_mean = vae.recon(vis_batch).view(-1, cfg.num_chan, cfg.image_size, cfg.image_size)
+            x_mean = vae.recon(vis_batch).cpu().view(-1, cfg.num_chan, cfg.image_size, cfg.image_size)
             vis.images(frames_to_rgb_list(x_mean), nrow=10)
-
-    if args.cuda:
-        batches = batches.cuda()
 
     run_svi(vae, batches, args.epochs, hook, output_path, args.s, elbo_scale=1.0/batch_size)
 
