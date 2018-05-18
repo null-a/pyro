256c5031d5b687eaddf9ad769d9e01ff33301f49
null-a <null-a@users.noreply.github.com>
2018-05-18 12:10:37 +0100

    Add ResNet modules.

--- examples/dynair/modules.py
+++ examples/dynair/modules.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.functional import relu
 
 # A general purpose module to construct networks that look like:
 # [Linear (256 -> 1)]
@@ -20,6 +21,38 @@ class MLP(nn.Module):
         if output_non_linearity:
             layers.append(non_linear_layer())
         self.seq = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.seq(x)
+
+
+# This implements the original formulation of ResNets.
+class ResidualBlockFC(nn.Module):
+    def __init__(self, size):
+        super(ResidualBlockFC, self).__init__()
+        in_size, hid_size, out_size = size
+        self.l1 = nn.Linear(in_size, hid_size)
+        self.l2 = nn.Linear(hid_size, out_size)
+        if in_size != out_size:
+            self.w = nn.Linear(in_size, out_size, bias=False)
+        else:
+            self.w = lambda x: x
+
+    def forward(self, x):
+        h = relu(self.l1(x))
+        return relu(self.l2(h) + self.w(x))
+
+
+
+class ResNet(nn.Module):
+    def __init__(self, in_size, hids, block=ResidualBlockFC):
+        super(ResNet, self).__init__()
+        assert len(hids) % 2 == 0, 'number of hidden layers must be even'
+        num_blocks = len(hids) // 2
+        all_sizes = [in_size] + hids
+        block_sizes = [tuple(all_sizes[(i*2):(i*2)+3]) for i in range(num_blocks)]
+        blocks = [block(size) for size in block_sizes]
+        self.seq = nn.Sequential(*blocks)
 
     def forward(self, x):
         return self.seq(x)
