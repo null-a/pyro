5ad7b9ffaa509f1830182c340962329eacb33b43
null-a <null-a@users.noreply.github.com>
2018-06-26 15:57:30 +0100

    Add script to download all TR data.

---
+++ examples/dynair/data/fetch_all_tr.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+`dirname $0`/fetch_bkg_tr_obj11_s.sh
+`dirname $0`/fetch_bkg_tr_obj13_tr.sh
+`dirname $0`/fetch_tr_obj11_s.sh
+`dirname $0`/fetch_tr_obj11_t.sh
+`dirname $0`/fetch_tr_obj11_tr.sh
+`dirname $0`/fetch_tr_obj13_tr.sh
