c646aae7a50ccf8fb13e44bae08ca4463c19c225
null-a <null-a@users.noreply.github.com>
2019-02-19 15:41:05 +0000

    Noodling with basic wake phase guide update.

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-#import torch.functional as F
+from torch.nn.functional import softplus
 
 import pyro
 import pyro.distributions as dist
@@ -21,7 +21,7 @@ import pyro.optim
 
 
 def model(obs):
-    for i in pyro.plate('data', 2):
+    for i in pyro.plate('data', len(obs)):
         z = pyro.sample('z%d' % i, dist.Bernoulli(torch.tensor(0.9)))
         x = pyro.sample('x%d' % i, dist.Bernoulli(torch.tensor(0.6 if z else 0.3)), obs=obs[i])
 
@@ -31,13 +31,13 @@ def zProb(obs):
     return torch.sigmoid(p1 * obs + p2)
 
 def guide(obs):
-    for i in pyro.plate('data', 2):
+    for i in pyro.plate('data', len(obs)):
         z = pyro.sample('z%d' % i, dist.Bernoulli(zProb(obs[i])))
 
-optimizer = pyro.optim.Adam({"lr": 0.01})
-svi = pyro.infer.SVI(model, guide, optimizer, loss=pyro.infer.Trace_ELBO())
+# optimizer = pyro.optim.Adam({"lr": 0.01})
+# svi = pyro.infer.SVI(model, guide, optimizer, loss=pyro.infer.Trace_ELBO())
 
-data = [torch.tensor(0.0), torch.tensor(1.0)]
+# data = [torch.tensor(0.0), torch.tensor(1.0)]
 
 # for _ in range(2000):
 #     svi.step(data)
@@ -47,13 +47,10 @@ data = [torch.tensor(0.0), torch.tensor(1.0)]
 
 
 
-
-# TODO: Need to think about the p_D distribution, and sub-sampling.
-
 def wake_guide_update(model, guide, *args, **kwargs):
     surrogate = 0.0
     ws = []
-    for i in range(10):
+    for i in range(100):
         guide_trace = pyro.poutine.trace(guide).get_trace(*args, **kwargs)
         model_trace = pyro.poutine.trace(
             pyro.poutine.replay(model, trace=guide_trace)).get_trace(*args, **kwargs)
@@ -62,14 +59,49 @@ def wake_guide_update(model, guide, *args, **kwargs):
         w = torch.exp(logp - logq).detach()
         surrogate += w * (-logq)
         ws.append(w)
+    # print(ws)
+    # print(sum(ws))
     return surrogate / sum(ws)
 
 
 
-svi = pyro.infer.SVI(model, guide, optimizer, loss=wake_guide_update)
+# svi = pyro.infer.SVI(model, guide, optimizer, loss=wake_guide_update)
 
-for _ in range(1000):
-    svi.step(data)
+# for _ in range(2000):
+#     svi.step(data)
 
-print(zProb(data[1]).detach()) # 0.9473684210526315
-print(zProb(data[0]).detach()) # 0.8372093023255814
+# print(zProb(data[1]).detach()) # 0.9473684210526315
+# print(zProb(data[0]).detach()) # 0.8372093023255814
+
+
+
+# Attempting to observe the mode covering behaviour of guides
+# optimised with this KL. This kinda works, but it appears to require
+# (a) lots of samples per gradient step for stability, an not too big
+# learning rate. (1e-3 seems ok, 1e-2 doesn't seem to do the right
+# things.)
+def model2():
+    z = pyro.sample('x', dist.MultivariateNormal(torch.tensor([0., 0]), torch.tensor([[1, 0.9], [0.9, 1]])))
+
+# This is a mean field guide. i.e. The two components of z are
+# independent, and this can't capture the dependency of the zs in the
+# model.
+def guide2():
+    cov = torch.diag(torch.exp(pyro.param('sd', torch.log(torch.tensor([1., 1.])))))
+    z = pyro.sample('x', dist.MultivariateNormal(torch.tensor([0., 0]), cov))
+
+
+optimizer = pyro.optim.Adam({"lr": 0.001})
+
+#svi = pyro.infer.SVI(model2, guide2, optimizer, loss=pyro.infer.Trace_ELBO())
+svi = pyro.infer.SVI(model2, guide2, optimizer, loss=wake_guide_update)
+
+for i in range(200):
+    loss = svi.step()
+    if (i+1) % 1 == 0:
+        print(loss)
+
+print(torch.exp(pyro.param('sd')).detach())
+
+
+# TODO: Try trad. sleep phase guide update.
