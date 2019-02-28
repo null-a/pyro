21dcf2f3dfe0d28d070e944a6f86777df92c8207
null-a <null-a@users.noreply.github.com>
2019-02-28 11:16:51 +0000

    Update script to reflect what was run.

--- examples/vae/sbn_exp1/run_sbn_exp.sh
+++ examples/vae/sbn_exp1/run_sbn_exp.sh
@@ -2,12 +2,12 @@
 
 for i in {1..1}
 do
-    python3 sbn.py -n 1 -tf 1 --save --loss elbo --baseline none
+    python3 sbn.py --cuda -n 1500 -tf 15 --save --loss elbo --baseline none
     mv history.json elbo_$i.json
-    python3 sbn.py -n 1 -tf 1 --save --loss elbo --baseline avg
+    python3 sbn.py --cuda -n 1500 -tf 15 --save --loss elbo --baseline avg
     mv history.json elbo_avg_$i.json
-    python3 sbn.py -n 1 -tf 1 --save --loss elbo --baseline net
+    python3 sbn.py --cuda -n 1500 -tf 15 --save --loss elbo --baseline net
     mv history.json elbo_net_$i.json
-    python3 sbn.py -n 1 -tf 1 --save --loss rws
+    python3 sbn.py --cuda -n 500 -tf 5 --save --loss rws
     mv history.json rws_$i.json
 done
