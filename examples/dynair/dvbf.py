6221f201de7ae43f78769e3c0237926848b18d49
null-a <null-a@users.noreply.github.com>
2019-04-01 09:36:07 +0100

    Don't backprop through transitions from the guide.

--- examples/dynair/dvbf.py
+++ examples/dynair/dvbf.py
@@ -208,13 +208,13 @@ class Guide(nn.Module):
                     # TODO: This duplicates computation, since we also
                     # transition from z_prev below. I ought to improve
                     # efficiency if I stick with this.
-                    z_prop = self.transition(t, z_prev, None, deterministic=True)
+                    z_prop = self.transition(t, z_prev, None, deterministic=True).detach()
 
                     w_mean, w_sd = self.predict_net(torch.cat((x, z_prev, z_prop), 1))
 
                 w = pyro.sample('w_{}'.format(t),
                                 dist.Normal(w_mean, w_sd).independent(1))
-                z = self.transition(t, z_prev, w)
+                z = self.transition(t, z_prev, w).detach()
                 z_prev = z
 
 
