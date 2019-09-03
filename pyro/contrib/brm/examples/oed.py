9a67ca221e253587cb5bdb39daf78ebad67aaa1a
null-a <null-a@users.noreply.github.com>
2019-09-03 14:46:59 +0100

    Add ability to use q net that doesn't assume indep. of targets.

--- pyro/contrib/brm/examples/oed.py
+++ pyro/contrib/brm/examples/oed.py
@@ -3,6 +3,7 @@ from pandas.api.types import is_categorical_dtype
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.nn.functional import one_hot
 import numpy as np
 
 from matplotlib import pyplot as plt
@@ -85,7 +86,8 @@ def next_trial(formula, model_desc, data_so_far, meta, verbose=False):
         inputs_d = inputs[N*j:N*(j+1)]
         targets_d = targets[N*j:N*(j+1)]
 
-        q_net = QIndep(num_coefs)
+        #q_net = QIndep(num_coefs)
+        q_net = QFull(num_coefs)
         optimise(q_net, inputs_d, targets_d, verbose)
 
         # Make a picture of the training data.
@@ -99,7 +101,7 @@ def next_trial(formula, model_desc, data_so_far, meta, verbose=False):
             imin = inputs_d.min()
             imax = inputs_d.max()
             test_in = torch.arange(imin, imax, (imax-imin)/50.).reshape(-1, 1)
-            test_out = q_net.marginal_logprobs(test_in, k).detach()
+            test_out = q_net.marginal_probs(test_in, k).detach()
 
             plot_data[j][k] = (pos_cases.numpy(), neg_cases.numpy(), test_in.numpy(), test_out.numpy(), design)
 
@@ -194,12 +196,70 @@ class QIndep(nn.Module):
     # being within [-eps,eps]. For this particular Q (which assumes
     # the joint is the product of the marginals) this only requires us
     # to pick out the appropriate marginal.
-    def marginal_logprobs(self, inputs, coef):
+    def marginal_probs(self, inputs, coef):
         assert type(coef) == int
         assert 0 <= coef < self.num_coef
         probs = self.forward(inputs)
         return probs[:,coef]
 
+
+# e.g. tensor([[0,0,1], [1,1,0]]) => tensor([1,6])
+def bits2long(t):
+    N, width = t.shape
+    powers_of_two = torch.tensor([2**i for i in range(width-1, -1, -1)])
+    out = torch.sum(t * powers_of_two, 1)
+    assert out.shape == (N,)
+    return out
+
+# e.g. (3,4) => [0,0,1,1]
+def int2bits(i, width):
+    assert i < 2**width
+    return [int(b) for b in ('{:0'+str(width)+'b}').format(i)]
+
+# All of the target values (as bit vectors) that satisfy \theta_coef == 1
+def target_values_for_marginal(coef, num_coef):
+    #print(list(int2bits(i, num_coef) for i in range(2**num_coef)))
+    values = [bits for bits in (int2bits(i, num_coef) for i in range(2**num_coef)) if bits[coef] == 1]
+    #print(values)
+    return torch.tensor(values)
+
+# e.g. [[1,0,1],[0,0,1]] => [[0,0,0,0,0,1,0,0],[0,1,0,0,0,0,0,0]]
+def bits2onehot(t):
+    width = t.shape[1]
+    return one_hot(bits2long(t), 2**width)
+
+class QFull(nn.Module):
+    def __init__(self, num_coef):
+        super(QFull, self).__init__()
+        assert type(num_coef) == int
+        assert num_coef > 0
+        self.num_coef = num_coef
+        self.net = nn.Sequential(nn.Linear(1, 100),
+                                 nn.ReLU(),
+                                 nn.Linear(100, 50),
+                                 nn.ReLU(),
+                                 nn.Linear(50, 2**num_coef),
+                                 nn.LogSoftmax(dim=1))
+
+    def forward(self, inputs):
+        assert inputs.shape[1] == 1
+        return self.net(inputs)
+
+    def logprobs(self, inputs, targets):
+        assert inputs.shape[0] == targets.shape[0]
+        N = inputs.shape[0]
+        assert inputs.shape == (N, 1)
+        assert targets.shape == (N, self.num_coef)
+        logprobs = self.forward(inputs)
+        assert logprobs.shape[1] == 2 ** self.num_coef
+        return torch.sum(logprobs * bits2onehot(targets.long()).float(), 1)
+
+    def marginal_probs(self, inputs, coef):
+        assert type(coef) == int
+        assert 0 <= coef < self.num_coef
+        logprobs = self.forward(inputs)
+        cols = bits2long(target_values_for_marginal(coef, self.num_coef))
+        return torch.sum(torch.exp(logprobs[:,cols]), 1)
 
 
 def df_append_row(df, row):
